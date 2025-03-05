13fe48e83a202b6f96080667b3e214f5c0c5c575
rickytheoldtree <rickyliyiqi@outlook.com>
2025-03-05 11:46:45 +0800

    v1.2.2

---
+++ Editor/DictConvertor/IDictConverter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RicKit.Localization.DictConvertor
+{
+    public interface IDictConverter
+    {
+        Dictionary<string,string> Convert(string json);
+        string Convert(Dictionary<string,string> dict);
+    }
+}
